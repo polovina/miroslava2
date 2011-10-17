899d66d084016a88b72209d73de3c5dbeeba9f59
alex.polovinko@gmail.com <alex.polovinko@gmail.com>
2011-10-17 13:53:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miroslava2::Application.config.secret_token = '5288d4400b9cfebc047d3abfe54803185cc48ad6e6e3141ee7fa9dd2a68fcba415724ab4d9de66d9fa4f16d96d378bcbe33c68d7ad21b2472bc630663b85dd90'
